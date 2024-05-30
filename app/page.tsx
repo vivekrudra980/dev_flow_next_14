8774d4394113858c5b2da259afe23ed7015136b0
vivekrudra980 <vivekprudra@gmail.com>
2024-05-30 20:17:42 +0530

    add assets and layout

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">Welcome to DevFlow</h1>
-      <h2 className="h2-bold">Welcome to DevFlow</h2>
-      <h3 className="h3-bold">Welcome to DevFlow</h3>
-    </div>
-  );
-}
